4a4566878f2a67ee85583dc84654d7722f6d3f7d
maksim <sairus.89@mail.ru>
2022-10-24 15:25:59 +0300

    accordion added

---
+++ accordeon/accordeon.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let acc = new Accordion('.accordion__list', {
+    duration: 700,
+    elementClass: 'accordion__item',
+    triggerClass: 'accordion__top',
+    panelClass: 'accordion__bottom',
+    showMultiple: false,
+    openOnInit: [0],
+      collapse: false
+  });
+  })
